a036c329586a0f35ae34a0b635da30625f01e700
Michalina08 <125758811+Michalina08@users.noreply.github.com>
2023-10-15 18:00:57 +0200

    Aplikacja 12,Komunikacja. Wszystko działa, oceny dobrze sie  pokazuja

--- StudentJournal/StudentJournal/Program.cs
+++ StudentJournal/StudentJournal/Program.cs
@@ -1,47 +1,26 @@
-﻿Student student1 = new Student("kasia", "Nwak", "15lat");
+﻿using StudentJournal;
+
+Console.WriteLine("Witamy w dziennieku ucznia !");
+Console.WriteLine("============================");
+Console.WriteLine();
+Console.WriteLine("Podaj swoje oceny z Matematyki:");
+
+var inputMath = Console.ReadLine();
+var student = new StudentBase();
+student.AddGradeMath(inputMath);
+var statisticsMath = student.GetStatisticsMath();
+Console.WriteLine(statisticsMath.Average);
+Console.WriteLine(statisticsMath.Min);
+
+Console.WriteLine("Witamy w dziennieku ucznia !");
+Console.WriteLine("============================");
+Console.WriteLine();
+Console.WriteLine("Podaj swoje oceny z Matematyki:");
 
 
-student1.AddScore(4);
-student1.AddScore(4);
-var result = student1.Result;
-Console.WriteLine(result);
-var result2 = student1.Result2;
-Console.WriteLine(result2);
-
-class Student
-{
-    private List<int> score = new List<int>();
-    private List<int> score2 = new List<int>();
-    public Student(string login, string surname, string age)
-    {
-        this.Login = login;
-        this.Surname = surname;
-        this.Age = age;
-     
-
-    }
-    public string Login { get; private set; }
-    public string Surname { get; private set; }
-    public string Age { get; private set; }
-    public int Result
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-    public int Result2
-    {
-        get
-        {
-            return this.score2.Sum();
-        }
-    }
-
-    public void AddScore(int number)
-    {
-        this.score.Add(number);
-        this.score2.Add(number);
-        
-    }
-}
+var inputIT = Console.ReadLine();
+var student2 = new StudentBase();
+student2.AddGradeMath(inputIT);
+var statisticsIT = student2.GetStatisticsMath();
+Console.WriteLine(statisticsIT.Average);
+Console.WriteLine(statisticsIT.Min);
---
+++ StudentJournal/StudentJournal/Statistics.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentJournal
+{
+    public class Statistics
+    {
+        public float Min { get; set; }
+        public float Max { get; set; }
+        public float Average { get; set; }
+        
+    }
+}
